36d5e5e905509624628b935d3791e5df0a448e50
shadow <61777185+oneshadoes@users.noreply.github.com>
2023-07-18 18:01:00 +0800

    父标签完成
    父标签完成，接下来是子标签，但还在看。https://www.bilibili.com/video/BV1b3411G7wL/?p=15&spm_id_from=pageDriver&vd_source=54845aafc5665a4b687d1a53d7cea93b

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,11 +1,19 @@
 <?php 
 function university_files() {
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     // wp_enqueue_style('font-awesome', get_theme_file_uri('css/maxcdn.bootstrapcdn.com_font-awesome_4.7.0_css_font-awesome.min.css'));
     //可以这样引用，但看不到图标
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    //引入css文件
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
 add_action('wp_enqueue_scripts','university_files'); 
 //这里名字不要输错.第二个函数随便起名
+
+function university_features() {
+    add_theme_support('title-tag');
+
+}
+add_action('after_setup_theme','university_features'); 
--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -1,11 +1,51 @@
 <?php
+get_header();
 while(have_posts()){
     the_post();
 ?>
-    <p>这是一个帖子，一个单独的页面</p>
-    <h5>这是最新的文章:</h5><h2><?php the_title(); ?></h2>
-    <?php the_content();?>
-    <!-- <hr> -->
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title();?></h1>
+        <div class="page-banner__intro">
+          <p>Learn how the school of your dreams got started.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+        <?php 
+        // echo get_the_ID();
+        //输出和下面父id一样   echo wp_get_post_parent_id();
+        // echo wp_get_post_parent_id(get_the_ID());
+        $theParent = wp_get_post_parent_id(get_the_ID());
+        if ($theParent) {?>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent);?>">
+          <i class="fa fa-home" aria-hidden="true"></i>
+           Back to <?php echo get_the_title($theParent); ?></a>
+            <!--echo 输出和下面the_title不一样，前面的函数可以填数字id值  -->
+           <span class="metabox__main"><?php the_title();?></span>
+        </p>
+      </div>
+        <?php }
+        ?>
+
+
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <ul class="min-list">
+          <li class="current_page_item"><a href="#"><?php the_title();?></a></li>
+          <li><a href="#">Our Goals</a></li>
+        </ul>
+      </div>
+
+      <div class="generic-content">
+<?php the_content(); ?>
+      </div>
+    </div>
 <?php 
 }
+get_footer();
 ?>
